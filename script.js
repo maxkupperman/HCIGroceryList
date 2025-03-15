08dd6f06458d4aabee3146f0b736d3eeb54e4699
Max <max.kupperman@gmail.com>
2025-03-15 19:55:33 -0400

    add share button, add recipe text area, default text

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
+function hideMainArea(document){
+    document.style['width'] = '0%';
+    document.style['overflow-x'] = 'hidden';
+}
+
+function showMainArea(document){
+    document.style['width'] = '100%';
+    document.style['overflow-x'] = 'visible';
+}
+
+function highlightButton(document){
+    document.style['background-color'] = '#6aa84fff';
+    document.style['color'] = '#ffffff';
+}
+
+function unHighlightButton(document){
+    document.style['background-color'] = '#ffffff';
+    document.style['color'] = '#6aa84fff';
+}
+
 function toRecipes(){
     const recipes = document.getElementById("recipes");
-    recipes.style['width'] = '100%';
-    recipes.style['overflow-x'] = 'visible';
+    showMainArea(recipes)
 
     const list = document.getElementById('list');
-    list.style['width'] = '0%';
-    list.style['overflow-x'] = 'hidden';
+    hideMainArea(list)
+
+    const recipeButton = document.getElementById("recipeButton");
+    highlightButton(recipeButton);
+
+    const listButton = document.getElementById("listButton");
+    unHighlightButton(listButton)
+
+    document.getElementById("listImage").src = 'img/cartGreen.png';
+    document.getElementById("recipeImage").src = 'img/hatWhite.png';
 };
 
 function toList(){
     const recipes = document.getElementById("recipes");
-    recipes.style['width'] = '0%';
-    recipes.style['overflow-x'] = 'hidden';
+    hideMainArea(recipes)
 
     const list = document.getElementById('list');
-    list.style['width'] = '0%';
-    list.style['overflow-x'] = 'hidden';
+    showMainArea(list)
+
+    const recipeButton = document.getElementById("recipeButton");
+    unHighlightButton(recipeButton);
+
+    const listButton = document.getElementById("listButton");
+    highlightButton(listButton)
+
+    document.getElementById("listImage").src = 'img/cartWhite.png';
+    document.getElementById("recipeImage").src = 'img/hatGreen.png';
 };
