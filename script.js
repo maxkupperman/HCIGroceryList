51f3e19d394e9919fa96f3a457e2110a8242c877
Max <max.kupperman@gmail.com>
2025-03-15 17:05:38 -0400

    get scrolling working right, add button functions

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function toRecipes(){
+    const recipes = document.getElementById("recipes");
+    recipes.style['width'] = '100%';
+    recipes.style['overflow-x'] = 'visible';
 
+    const list = document.getElementById('list');
+    list.style['width'] = '0%';
+    list.style['overflow-x'] = 'hidden';
+};
+
+function toList(){
+    const recipes = document.getElementById("recipes");
+    recipes.style['width'] = '0%';
+    recipes.style['overflow-x'] = 'hidden';
+
+    const list = document.getElementById('list');
+    list.style['width'] = '0%';
+    list.style['overflow-x'] = 'hidden';
 };
