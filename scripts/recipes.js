6e43bf8e362ecd6c865187004f767b05b2e42d3a
Max <max.kupperman@gmail.com>
2025-03-16 12:41:33 -0400

    make recipes work

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -16,5 +16,30 @@ function hideOrShowIngredients(selector) {
 }
 
 function selectOrUnselectRecipe(selfSelector, ingredientsSelector) {
-    
+    var checkbox = document.getElementById(selfSelector)
+    var ingredientList = document.getElementById(ingredientsSelector)
+    var mainList = document.getElementById('listText')
+
+    if(checkbox.classList.contains('unselected')){
+        checkbox.classList.remove('unselected')
+        checkbox.classList.add('selected')
+        checkbox.src = 'img/boxChecked.png'
+
+        mainList.innerHTML += ingredientList.innerHTML
+    } 
+    else if (checkbox.classList.contains('selected')){
+        var recipeItems = mainList.getElementsByClassName(ingredientsSelector)
+        console.log(recipeItems)
+
+        while (recipeItems.length > 0){
+            recipeItems[0].parentNode.removeChild(recipeItems[0])
+            console.log('removed')
+        }
+
+        checkbox.classList.remove('selected')
+        checkbox.classList.add('unselected')
+        checkbox.src = 'img/boxUnchecked.png'
+
+
+    }
 }
