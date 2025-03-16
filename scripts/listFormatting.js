9556d4ec964854e217161b5063a7ce47e9067b35
Max <max.kupperman@gmail.com>
2025-03-15 23:37:11 -0400

    get suggestions logging to console plus reminders

--- scripts/listFormatting.js
+++ scripts/listFormatting.js
@@ -1,7 +1,12 @@
+const suggestionPosition = 1
+const suggestionList = ['Bananas','Apples']
+
 function listFormatting() {
     console.log('its running')
 };
 
-function addSuggestion() {
-
+function addSuggestion(selector) {
+    var suggestion = document.getElementById(selector);
+    var content = suggestion.innerHTML;
+    console.log(content)
 }
