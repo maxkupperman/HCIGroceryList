4bcc1ee3e4b53485af04c3cf1c7d475776155a7a
Max <max.kupperman@gmail.com>
2025-03-15 20:38:48 -0400

    add alerts for the menu and share icons

---
+++ scripts/alerts.js
@@ -0,0 +1,7 @@
+function shareAlert(){
+    alert("In an app that actually had a backend, this would allow you to share your grocery list.\n But right now, it doesn't do anything.")
+};
+
+function menuAlert() {
+    alert("In a more fully built out app, this menu would allow you to:\n * View past lists\n * Access settings\nBut right now, it doesn't do anything.")
+}
